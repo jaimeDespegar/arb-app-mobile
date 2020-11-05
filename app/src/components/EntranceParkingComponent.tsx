4871760c1398c9b9b4d59e2ecfcd22ba4c8353ec
jaimeDespegar <jaime.quispe.1994@outlook.com>
2020-11-05 17:47:16 -0300

    entrada componente

--- app/src/components/EntranceParkingComponent.tsx
+++ app/src/components/EntranceParkingComponent.tsx
@@ -1,136 +1,119 @@
-//Vincularlo con la API (post-get-put)
-//Validaciones
-//Se deja para lo último 
-
 import * as React from 'react';
 import { useState, useEffect } from "react"; 
-import {  StyleSheet, View, Text, Picker } from 'react-native';
-import { useTheme, Button, } from 'react-native-paper';
-import { inputReducer } from '../../utils';
+import { StyleSheet, View, Text, Picker } from 'react-native';
+import { Button, } from 'react-native-paper';
 import { DialogWithCustomColors } from './Dialogs';
 import DialogCustom from './Dialogs/DialogCustom'
 import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-//import {AsyncStorage} from 'react-native';
-
-
-const initialState = {
-    text: '',
-    userName:'',
-};
+import { loadValue, USER_KEY } from './utils/StorageHelper'
 
   
 const EntranceParkingComponent = () => {
-    //Hacerlo desde la API a la relacion
+  
+  const [data, setData] = useState([]);  
+  
+  const [userNameLogin, setUserNameLogin] = useState("");
+  const [titleMessage, setTitleMessage] = useState("");
+  const [contentMessage, setContentMessage] = useState("");
 
-    
-    const STORAGE_KEY = 'userName'
+  const [showMessage, setShowMessage] = React.useState(false);
+  const showDialog = () => { setShowMessage(true) };
+  const hideDialog = () => setShowMessage(false);
 
-    const [userNameLogin, setUserNameLogin]=  useState("");
-    const  load = async () => {
-      try {
-        let userAux= await AsyncStorage.getItem(STORAGE_KEY);
-        setUserNameLogin(userAux)
-        //alert(userNameLogin);
-    
-      } catch (e) {
-        console.error('Failed to load .')
-      }
+  const parkTheBike = () => {
+
+    const values = {
+      'userName': userNameLogin,
+      'place': selectedValue
     }
-    
 
-  const [state, dispatch] = React.useReducer(inputReducer, initialState);
-  const {
-    text,userName,photoPath,place,isOk,isSuspected,estadia
-  } = state;
+    axios
+      .post('parking/entrance/', values)
+      .then(response => {
+        setTitleMessage("Estacionamiento Exitoso")
+        setContentMessage("Ahora puede ir a cursar seguro")
+        showDialog();
+      })
+      .catch(error => {
+        console.log('Error in entrace park ', error);
+        setTitleMessage("Error al intentar estacionar")
+        setContentMessage("Intente nuevamente mas tarde");
+        showDialog();
+      });
+  }
 
-  const { colors } = useTheme();
-
-  const inputActionHandler = (type: string, payload: string) =>
-    dispatch({
-      type: type,
-      payload: payload,
-    });
-  const  userName2= 'Pepe';
   const [visible, setVisible] = React.useState(false);
-  const [showErrorLocation, setShowErrorLocation] = React.useState(true);
 
   const _toggleDialog = (name: string) => () =>
     setVisible({ ...visible, [name]: !visible[name] });
 
   const _getVisible = (name: string) => !!visible[name];  
   
-  //(ComboBox) Picker
-  const [selectedValue, setSelectedValue] = useState("java");
-
-
-  //Alerta
-  const [data, setData] = useState([]);
+  const [selectedValue, setSelectedValue] = useState("1");
 
   useEffect(() => {
-    load() //AsyncStorage
-    axios  
-      .get('notificationEgress-getUser/'+userNameLogin+'/')
-      .then((response) => response.data)
-      .then((json) => setData(json)) //es el print()
-      .catch((error) => console.error('Error Entrance', error))
+      
+    loadValue(USER_KEY, setUserNameLogin);
 
-    if(data.userName === userNameLogin){
-      setVisible(true)
-      _getVisible('dialog5') 
-      console.log("tu bici esta en peligro!")
-    }
-    else{
-      console.log("tu bici esta ok")
-    } 
+    // axios
+    //   .get('notificationEgress-getUser/' + userNameLogin + '/')
+    //   .then((response) => response.data)
+    //   .then((json) => setData(json))
+    //   .catch((error) => console.error('Error Entrance', error));
+
+    //   if(data.userName === userNameLogin){
+    //     setVisible(true)
+    //     console.log("tu bici esta en peligro!")
+    //   }
+    //   else{
+    //     console.log("tu bici esta ok")
+    //   }
   }, [userNameLogin]);
-  //.get('notificationEgress-getUser/'+userName+'/')
-  const notificarAlerta = () => (data.userName == "userName")?  _toggleDialog('dialog5') : "OK"
-  const estacionarBici = () => {
-    console.log("Estacionar Bicicleta")
-  }
-  return (
-    <View style={styles.inputs}>
-      
-      <Text style={styles.goodBye}>
-        Seleccione su estacionamiento:
-      </Text>
-      <Picker
-        selectedValue={selectedValue}
-        style={{ height: 50, width: 150 }}
-        onValueChange={(itemValue, itemIndex) => {setSelectedValue(itemValue); console.log(itemValue)}}
-      >
-        <Picker.Item label="1" value="1" />
-        <Picker.Item label="2" value="2" />
-        <Picker.Item label="3" value="3" />
-        <Picker.Item label="4" value="4" />
-        <Picker.Item label="5" value="5" />
-        <Picker.Item label="6" value="6" />
-      </Picker>
-      <Text style={styles.goodBye}>
-        Bienvenido a la UNGS, {userNameLogin}!
-      </Text>
-      
-      <Button mode="contained" onPress={() => estacionarBici()} style={styles.button}>
-        Estacionar Bicicleta
-      </Button>
-      <Button mode="outlined" icon="alert" onPress={_toggleDialog('dialog5')}  style={styles.stylesContainer}>
-        Alerta Robo
-      </Button>
-      <DialogWithCustomColors
-        visible={visible}
-        close={_toggleDialog('dialog5')}
-        data={data}
-      />
-      {/* <DialogCustom
-              visible={showErrorLocation}
-              title='Estacionamiento no permitido'
-              content='Verifique que este cerca de un bicicletero'
-              messageAction='Ok'
-              close={()=>{setShowErrorLocation(false)}}
-       /> */}
-    </View>
-  );
+  
+    return (
+      <View style={styles.inputs}>
+        
+        <Text style={styles.goodBye}>
+          Seleccione su estacionamiento:
+        </Text>
+        <Picker
+          selectedValue={selectedValue}
+          style={{ height: 50, width: 150 }}
+          onValueChange={(itemValue, itemIndex) => {setSelectedValue(itemValue); console.log(itemValue)}}
+        >
+          <Picker.Item label="1" value="1" />
+          <Picker.Item label="2" value="2" />
+          <Picker.Item label="3" value="3" />
+          <Picker.Item label="4" value="4" />
+          <Picker.Item label="5" value="5" />
+          <Picker.Item label="6" value="6" />
+        </Picker>
+        <Text style={styles.goodBye}>
+          Bienvenido a la UNGS, {userNameLogin}!
+        </Text>
+        
+        <Button mode="contained" onPress={() => parkTheBike()} style={styles.button}>
+          Estacionar Bicicleta
+        </Button>
+        <Button mode="outlined" icon="alert" onPress={_toggleDialog('dialog5')}  style={styles.stylesContainer}>
+          Alerta Robo
+        </Button>
+        <DialogWithCustomColors
+          visible={false}
+          close={_toggleDialog('dialog5')}
+          data={data}
+        />
+        <View>
+          <DialogCustom
+            visible={showMessage}
+            title={titleMessage}
+            content={contentMessage}
+            messageAction='OK'
+            close={hideDialog}
+          />
+        </View>
+      </View>
+    );
 };
 
 EntranceParkingComponent.title = 'Entrance Parking';
