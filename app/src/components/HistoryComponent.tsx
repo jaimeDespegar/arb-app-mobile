65cafdd5bbe1defb42307fa665bfae0761787e2c
jaimeDespegar <jaime.quispe.1994@outlook.com>
2020-11-20 21:03:05 -0300

    modify history

--- app/src/components/HistoryComponent.tsx
+++ app/src/components/HistoryComponent.tsx
@@ -1,75 +1,175 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, FlatList, Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import { format } from 'date-fns';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { USER_KEY } from './utils/StorageHelper';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { USER_KEY, loadValue } from './utils/StorageHelper';
+import { TextInput, Button, List } from 'react-native-paper';
+import { inputReducer } from '../../utils';
 
-    
+
+const initialState = {
+  fromDate: new Date(),
+  toDate: new Date()
+};
+
 const HistoryComponent = () => {
-    //Hacerlo desde la API a la relacion
+
   const [userNameLogin, setUserNameLogin]=  useState("");
-  const  load = async () => {
-    try {
-      let userAux= await AsyncStorage.getItem(USER_KEY);
-      setUserNameLogin(userAux)
-  
-    } catch (e) {
-      console.debug('Failed to load .')
+  const [data, setData] = useState([]);
+  const [mode, setMode] = useState('date');
+  const [showFromDate, setShowFromDate] = useState(false);
+  const [showToDate, setShowToDate] = useState(false);
+  const [state, dispatch] = React.useReducer(inputReducer, initialState);
+
+  const inputActionHandler = (type: string, payload: string) =>
+    dispatch({
+      type: type,
+      payload: payload,
+  });
+
+  const parseDate = (date, formatDate) => format(new Date(date), formatDate);
+
+  const {
+    fromDate,
+    toDate
+  } = state;
+
+  const onChangeFromDate = (event, selectedDate) => {
+    const currentDate = selectedDate || fromDate;
+    setShowFromDate(false);
+    inputActionHandler('fromDate', currentDate);
+    console.log('from date ', currentDate)
+  };
+
+  const onChangeToDate = (event, selectedDate) => {
+    const currentDate = selectedDate || toDate;
+    setShowToDate(false);
+    inputActionHandler('toDate', currentDate);
+    console.log('to date ', currentDate)
+  };
+
+  const showModeFromDate = () => {
+    setShowFromDate(true);
+    setMode('date');
+  };
+
+  const showModeToDate = () => {
+    setShowToDate(true);
+    setMode('date');
+  };
+
+  const findStays = (fromDate, toDate) => {
+    
+    let urlToGetStays = 'estadia/find?userName='+userNameLogin+'&isActive=false';
+
+    if (fromDate && toDate) {
+      urlToGetStays += '&fromDate=' + parseDate(fromDate, 'yyyy-MM-dd 00:00:00') + '&toDate=' + parseDate(toDate, 'yyyy-MM-dd 23:59:59');  
+    }
+
+    if (userNameLogin) {
+      axios
+      .get(urlToGetStays)
+      .then(json => {
+        console.log('ok estadia ', json.data)
+        setData(json.data)
+      })
+      .catch((error) => {
+        console.log('error estadia history ',userNameLogin);
+        setData([])
+      })
+    } else {
+      console.log('usuario no cargado')
     }
   }
 
-  const [isLoading, setLoading] = useState(true);
-  //CARGA DATOS EXISTENTES (estadía)
-  const [data, setData] = useState([]);
-  //.get('notificationEgress-getUser/'+userNameLogin+'/')
   useEffect(() => {
-    load() //AsyncStorage
-      if (userNameLogin) {
-        axios
-        .get('estadia/find?userName='+userNameLogin+'&isActive=false')
-        .then(json => {
-          console.log('ok estadia ', json.data)
-          setData(json.data)
-          setLoading(false)
-          })
-        .catch((error) => {
-          console.log('error estadia history ',userNameLogin);
-          setLoading(false);
-          setData([])
-        })
-      } else {
-        console.log('usuario no cargado')
-      }
-    }, [userNameLogin]);
+    loadValue(USER_KEY, setUserNameLogin);
+    findStays('', '');
+  }, [userNameLogin]);
   
-  const separador = () => {
-    return(
-        <View style={{
-            height: 5,
-            width: "100%",
-            backgroundColor: "black",
-            marginVertical: 10
-        }}>
-        </View>
-    )
-  }
-  //<Text> {data.placeUsed} | {data.dateCreated} | {data.userName}</Text>
   return (
     <View style={{ flex: 1, padding: 24 }}>
-      {isLoading ? <ActivityIndicator/> : (
-        <FlatList
-          data={data}
-          keyExtractor={({ id }, index) => id}
-          renderItem={({ item }) => (
-            <Text>{item.placeUsed.toString()} | {format(new Date(item.dateCreated), 'dd-MM-yyyy HH:mm:sss')} | {item.userName} </Text>
-          )}
-          horizontal= {false}
-          ItemSeparatorComponent=  {() => separador()}
-          ListEmptyComponent={<Text style={{marginTop:30 , textAlign: 'center'}}>
-               No hay estadías registradas</Text>}
+      <View>
+        <TextInput
+          label="Desde"
+          placeholder="Seleccione la fecha desde"
+          value={format(fromDate, '00:00:00 dd-MM-yyyy')}
+          onChangeText={(value) => inputActionHandler('fromDate',value)}
+          disabled={true}
+          right={
+            <TextInput.Icon
+              name={'calendar'}
+              onPress={showModeFromDate}
+              forceTextInputFocus={false}
+            />
+          }
         />
-      )}
+        <TextInput
+          label={"Hasta"}
+          placeholder={"Seleccione la fecha hasta"}
+          value={format(toDate, '23:59:59 dd-MM-yyyy')}
+          onChangeText={(value) => inputActionHandler('toDate',value)}
+          disabled={true}
+          right={
+            <TextInput.Icon
+              name={'calendar'}
+              onPress={showModeToDate}
+              forceTextInputFocus={false}
+            />
+          }
+        />
+        <Button mode="outlined" 
+                onPress={() => {findStays(fromDate, toDate)}} 
+                style={styles.button}>
+          Buscar Estadias
+        </Button>
+      </View>
+      <View>
+        {showFromDate && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={fromDate}
+            mode={mode}
+            is24Hour={true}
+            display="default"
+            dateFormat='yyyy-MM-dd HH:mm:ss'
+            onChange={onChangeFromDate}
+          />
+        )}
+        {showToDate && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={toDate}
+            mode={mode}
+            is24Hour={true}
+            display="default"
+            dateFormat='yyyy-MM-dd HH:mm:ss'
+            onChange={onChangeToDate}
+          />
+        )}
+      </View>
+      <View>
+        <List.Section title="Mis Estadías">
+          {
+          (!data.length) ? 
+          (
+            <Text style={{marginTop:30 , textAlign: 'center'}}> No hay estadías registradas</Text>
+          ) : 
+          data.map((item) => (
+            <List.Accordion
+              title={"Estadia en el lugar " + item.placeUsed.toString()}
+              description={'Generada el ' + parseDate(item.dateCreated, 'dd-MM-yyyy')}
+              left={props => <List.Icon {...props} icon="image-album" />}>
+              <List.Item title="Llegada" 
+                         description={'Estacionó a las ' + parseDate(item.arrival.dateCreated, 'HH:mm:ss')}/>
+              <List.Item title="Salida" 
+                         description={'Se retiró a las '+parseDate(item.departure.dateCreated, 'HH:mm:ss')}/>
+            </List.Accordion>
+          ))}
+
+        </List.Section>
+      </View>
     </View>
   );
 };
@@ -81,6 +181,11 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 100,
+  },
+  button: {
+    margin: 4,
+    height: 50,
+    justifyContent: 'center',
   },
 });
 
